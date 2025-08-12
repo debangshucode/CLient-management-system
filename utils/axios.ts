65d387d7872a70fef666afae317cfe40f4157549
Debangshu Dey <deydebangshu51@gmail.com>
2025-08-12 21:36:56 +0530

    axios

--- utils/axios.ts
+++ utils/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "", // adjust this as per your setup
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "", // adjust this as per your setup
   withCredentials: true, // this allows cookies to be sent (needed for session auth)
 })
 
